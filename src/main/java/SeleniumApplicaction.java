c88d2db1d39f05d06f693796dfbf9a0189b17a8e
Rajat sawant <rajatswnt@gmail.com>
2025-03-22 12:34:38 +0530

    Initial commit - open chrome

---
+++ src/main/java/SeleniumApplicaction.java
@@ -0,0 +1,8 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumApplicaction {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+    }
+}
